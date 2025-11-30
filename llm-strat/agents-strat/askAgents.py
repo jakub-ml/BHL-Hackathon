109ef26131ce690a188192da1338a63657d68a55
FitoruCode <wiktor.miskowiec1@gmail.com>
2025-11-30 01:32:45 +0100

    return WATTS
    Modified the return value of askAgents to include a conversion factor for watts.

--- llm-strat/agents-strat/askAgents.py
+++ llm-strat/agents-strat/askAgents.py
@@ -4,4 +4,6 @@ from openAIHandler import OpenAIHandler
 def askAgents(userProjectDescription):
     AIHandler = OpenAIHandler()
     response = AIHandler.userCallAI(userProjectDescription)
-    return response[0]['compute']
+    return response[0]['compute']*1/45426084950 #WATTS
+
+
